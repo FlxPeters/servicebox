bde63bd5651121c7f416a036b21e5af7e34b3ff7
Felix <felix.peters@breuninger.de>
2020-06-16 17:44:13 +0200

    fix debugtoolbar errors

--- servicebox/servicebox/urls.py
+++ servicebox/servicebox/urls.py
@@ -27,8 +27,3 @@ urlpatterns = [
     path("tenants/", include("tenancy.urls")),
     path("admin/", admin.site.urls),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [path("__debug__/", include(debug_toolbar.urls)),] + urlpatterns
