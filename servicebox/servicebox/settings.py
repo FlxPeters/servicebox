2250783673b40ec750258bd4a5d5595a7a6c2b7c
Felix Peters <mail@felixpeters.de>
2021-04-20 21:08:51 +0200

    Update settings.py

--- servicebox/servicebox/settings.py
+++ servicebox/servicebox/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "=1^42e!6&3c8ar8jn+6=5bsij_l9tx#$&pehtwpeufoy#j6-qa"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
